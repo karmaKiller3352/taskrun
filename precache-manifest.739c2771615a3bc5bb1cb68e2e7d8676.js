03cca2c5960c4e2e126ae5253d56ecde69b23570
karmaKiller3352 <alievsul@gmail.com>
2020-05-27 15:24:14 +0300

    Updates

---
+++ precache-manifest.739c2771615a3bc5bb1cb68e2e7d8676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76abc962d09c854b95feb4e63e692c97",
+    "url": "/taskrun/index.html"
+  },
+  {
+    "revision": "687f443b192fe557d100",
+    "url": "/taskrun/static/css/main.25570f4c.chunk.css"
+  },
+  {
+    "revision": "7eecc72db6689612d6aa",
+    "url": "/taskrun/static/js/2.752d2af0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/taskrun/static/js/2.752d2af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687f443b192fe557d100",
+    "url": "/taskrun/static/js/main.f0a37425.chunk.js"
+  },
+  {
+    "revision": "d7c8a9f69ac335fb69aa",
+    "url": "/taskrun/static/js/runtime-main.be43176b.js"
+  }
+]);
