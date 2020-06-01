7c693bbba482798e4462c94552a1627e1e794b01
karmaKiller3352 <alievsul@gmail.com>
2020-06-02 00:00:04 +0300

    Move timer set in useEffect

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -13,33 +13,25 @@ function Timer({ status, spentTime, start, getTime }) {
   const timerId = useRef(false);
 
   useEffect(() => {
+    if (status === 'IN_WORK') {
+      timerId.current = setTimeout(() => {
+        setTime((prevTime) => prevTime + 1000);
+      }, 1000);
+    } else {
+      clearTimeout(timerId.current);
+    }
+
     return () => {
       clearTimeout(timerId.current);
     };
-  }, []);
+  }, [status, time]);
+
   if (getTime) getTime(time);
 
-  switch (status) {
-    case 'FINISHED': {
-      clearTimeout(timerId.current);
-      return msToTime(spentTime);
-    }
-    case 'PAUSED': {
-      clearTimeout(timerId.current);
-      return msToTime(spentTime);
-    }
-    case 'IN_WORK': {
-      timerId.current = setTimeout(() => {
-        console.log(timerId.current);
-        setTime((prevTime) => prevTime + 1000);
-      }, 1000);
-      return msToTime(time);
-    }
-    default: {
-      clearTimeout(timerId.current);
-      return msToTime(time);
-    }
+  if (status === 'PAUSED' || status === 'FINISHED') {
+    return msToTime(spentTime);
   }
+  return msToTime(time);
 }
 
 export default Timer;
