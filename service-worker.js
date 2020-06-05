73f8d608a2edb8727d12200869487d9c5dcadcda
karmaKiller3352 <alievsul@gmail.com>
2020-06-06 01:06:39 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taskrun/precache-manifest.183d340dcc5c38d3257ddf40bf5b41bd.js"
+  "/taskrun/precache-manifest.6ffda977e68213534381e368150642b5.js"
 );
 
 self.addEventListener('message', (event) => {
